c8a3e24454bfd7ae754f89a9df59133d8847a54b
JibinAji21 <jibinaji26@gmail.com>
2024-12-09 13:37:27 +0530

    javascript started

---
+++ script.js
@@ -0,0 +1,213 @@
+//1) Alert>.........
+
+// alert("welcome to all")
+
+
+//2) Console...........
+
+// a="hello"
+// console.log("welcome to all",a);
+// alert(a)
+
+// var..........
+
+// var a="hello"
+// var a=20
+// console.log(a);
+
+
+// let...................
+
+// let a="hai"
+// a=25
+// console.log(a);
+
+
+// const.................
+
+// const a="welcome"
+// console.log(a);
+
+
+// DATATYPES IN JAVASCRIPT.................
+
+// 1) number type
+// 2) string type
+// 3) array type
+// 4) boolean type
+// 5) none type
+// 6) object type
+
+// let a
+// a= {name:'anu',age:20}
+// console.log(typeof a);
+
+// console.log(a);
+
+
+// OPERATORS IN JAVASCRIPT................
+
+// 1) Arithmetic operator:
+
+// console.log(10+7);
+// console.log(10-7);
+// console.log(10*7);
+
+// let a=10/5
+// console.log(a);
+
+
+// let a=10**5
+// console.log(a);
+
+
+// let a=5
+// a++
+// console.log(a);
+
+
+// let a=5
+// a--
+// console.log(a);
+
+
+// 2) Assignment operator:
+
+// console.log(a=10);
+
+
+// let a=10
+// a+=2
+// console.log(a);
+// output:12
+
+
+// let a=10
+// a-=2
+// console.log(a);
+// output:8
+
+
+// let a=10
+// a*=2
+// console.log(a);
+// output:20
+
+
+// let a=10
+// a/=2
+// console.log(a);
+// output=5
+
+
+// let a=10
+// a%=2
+// console.log(a);
+// output=0
+
+
+// let a=10
+// a**=2
+// console.log(a);
+// output=100
+
+// 3) Comparison operator:
+
+
+// let a=10
+// let b='20'
+// console.log(a==b);
+// output:false
+
+
+// let a=10
+// let b='10'
+// console.log(a==b);
+// output:true
+
+
+// let a=10
+// let b='10'
+// console.log(a!=b);
+// output:false
+
+
+// let a=10
+// let b='20'
+// console.log(a>b);
+// output:false
+
+
+// let a=10
+// let b='20'
+// console.log(a<b);
+// output:true
+
+
+// let a=10
+// let b='20'
+// console.log(a>=b);
+// output:false
+
+
+// let a=10
+// let b='20'
+// console.log(a<=b);
+// output:true
+
+
+// let a=10
+// let b='20'
+// console.log(a===b);
+// output:false
+
+
+// let a=10
+// let b=10
+// console.log(a===b);
+// output:true
+
+
+// let a=10
+// let b='20'
+// console.log(a!==b);
+// output:true
+
+
+// 4) Logical operator:
+
+// let a=10
+// let b="20"
+// console.log(a!=b&&a==10);
+// output:true
+
+
+// let a=10
+// let b="20"
+// console.log(a!=b||a==10);
+// output:true
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
