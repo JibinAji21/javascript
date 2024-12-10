0be937eb19e15eb58cdf84affaa2fc554625c423
JibinAji21 <jibinaji26@gmail.com>
2024-12-10 13:38:58 +0530

    javascript

--- script.js
+++ script.js
@@ -186,7 +186,110 @@
 // let b="20"
 // console.log(a!=b||a==10);
 // output:true
+// ....................................................................
 
+
+function sample(){
+
+
+    // let result=document.getElementById("name")
+    // console.log(result.value);
+    // document.getElementById("display").innerHTML=result.value
+
+
+    // let result=document.getElementById("name").value
+    // console.log(result);
+    // let h2=document.getElementById("display")
+    // h2.innerHTML=result
+    
+    // let no1=parseInt(document.getElementById("no1").value)
+    // let no2=parseInt(document.getElementById("no2").value)
+    // let h2=document.getElementById("display")
+    // h2.innerHTML=no1+no2
+
+
+    let no1=parseInt(document.getElementById("no1").value)
+    let no2=parseInt(document.getElementById("no2").value)
+    let h2=document.getElementById("display")
+
+    if(no1==no2){
+        h2.innerHTML='eql'
+    }
+    else if(no1!=no2){
+        h2.innerHTML='not eql'
+    }
+  
+
+
+
+
+    
+}
+
+function add(){
+    let no1=parseInt(document.getElementById("no1").value)
+    let no2=parseInt(document.getElementById("no2").value)
+    let h2=document.getElementById("display")
+    h2.innerHTML=no1+no2
+
+
+}
+
+function sub(){
+    let no1=parseInt(document.getElementById("no1").value)
+    let no2=parseInt(document.getElementById("no2").value)
+    let h2=document.getElementById("display")
+    h2.innerHTML=no1-no2
+
+
+}
+
+function mult(){
+    let no1=parseInt(document.getElementById("no1").value)
+    let no2=parseInt(document.getElementById("no2").value)
+    let h2=document.getElementById("display")
+    h2.innerHTML=no1*no2
+
+}
+
+function div(){
+    let no1=parseInt(document.getElementById("no1").value)
+    let no2=parseInt(document.getElementById("no2").value)
+    let h2=document.getElementById("display")
+    h2.innerHTML=no1/no2
+
+}
+
+function mod(){
+    let no1=parseInt(document.getElementById("no1").value)
+    let no2=parseInt(document.getElementById("no2").value)
+    let h2=document.getElementById("display")
+    h2.innerHTML=no1%no2
+
+    
+}
+
+// conditional statements tasks:
+
+function bonus(){
+    let salary=parseInt(document.getElementById("salary").value)
+    let year=parseInt(document.getElementById("year").value)
+    let h2=document.getElementById("display")
+
+    if(year>=5){
+        h2.innerHTML=salary*0.05+salary
+    }
+    else {
+        h2.innerHTML='no bonus'
+    }
+  
+
+}
+let year=parseInt(document.getElementById("num").value)
+let h2=document.getElementById("display")
+function divisible(){
+
+}
 
 
 
