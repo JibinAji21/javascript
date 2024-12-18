13eb9980b22aa59ee748461c2ecc74c83f96db75
JibinAji21 <JibinAji21>
2024-12-18 13:36:46 +0530

    validation using javascript

--- sample.js
+++ sample.js
@@ -53,17 +53,60 @@
 // },1000);
 
 
-let second=10
-let data = setInterval(()=> {
-        document.getElementById("time").innerHTML=second
-        console.log(second);
+// let second=10
+// let data = setInterval(()=> {
+//         document.getElementById("time").innerHTML=second
+//         console.log(second);
     
-        second--
-        if(second<0){
-            clearInterval(data)
-            if (second<0){
-                document.getElementById('image').style.display='none'
-            }
-        }
+//         second--
+//         if(second<0){
+//             clearInterval(data)
+//             if (second<0){
+//                 document.getElementById('image').style.display='none'
+//             }
+//         }
         
-    },1000);
+//     },1000);
+
+
+// document.getElementById("form").addEventListener("submit",function(event){
+//     event.preventDefault()
+//     let no = document.getElementById('no').value
+//     let a1 = document.getElementById("a1")
+//     if(/^[6-9]\d{9}/.test(no)){
+//         a1.innerHTML='valid'
+//     }
+//     else{
+//         a1.innerHTML='invalid'
+//     }
+// })
+
+
+
+let pswd=document.getElementById("pswd")
+let cap=document.getElementById("cap")
+let len=document.getElementById("len")
+let sm=document.getElementById("sm")
+let no=document.getElementById("no")
+let spl=document.getElementById("spl")
+pswd.addEventListener('input',function(){
+    let password = pswd.value
+    len.style.color=password.length>=8 ? 'green' : 'red'
+    cap.style.color=/[A-Z]/.test(password) ? 'green' : 'red'
+    sm.style.color=/[a-z]/.test(password) ? 'green' : 'red'
+    no.style.color=/\d/.test(password) ? 'green' : 'red'
+    spl.style.color=/[!@#$%&*]/.test(password) ? 'green' : 'red'
+})
+document.getElementById("form").addEventListener("submit",function(event){
+    event.preventDefault()
+    let password=document.getElementById("pswd").value
+    console.log(password.length);
+
+    if (password.length>=8  && /[A-Z]/.test(password) && /[a-z]/.test(password) && /\d/.test(password) && /[!@#$%&*]/.test(password) ){
+        alert('valid')
+    }
+    else{
+        alert('invalid')
+    }
+    
+})
